2fd0b5e3ba01a76cacf307b9201271bd9f63b135
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-06-27 03:02:24 +0200

    css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,56 +9,42 @@ import {
 
 export default function About() {
   return (
-    <main className="flex  flex-col items-center mt-10  p-3 sm:p-16">
-      <Card className="min-w-full sm:p-12">
-        <CardHeader>
-          <CardTitle>Fullstack web developer based in Paris</CardTitle>
-          <CardDescription></CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>
-                I mainly use NextJS, TypeScript, Tailwind and SQL database for
-                my work
-              </CardTitle>
-            </CardHeader>
-            <div className="flex justify-between">
-              <CardContent className="pt-6">
-                <p>
-                  Passionate about new tech i always look for the latest
-                  innovations in web development to make my apps beautiful, fast
-                  and the most secure possible. From a simple website to a
-                  complex app i will be happy to build it for you.
-                </p>
-              </CardContent>
-            </div>
-          </Card>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Contact</CardTitle>
-            </CardHeader>
-            <div className="flex justify-between">
-              <CardContent className="pt-6">
-                <p>Julien A.</p>
-                <p> jj.websolutions@proton.me </p>
-              </CardContent>
-            </div>
-          </Card>
-
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Web3</CardTitle>
-            </CardHeader>
-            <CardContent>
+    <main className="flex  flex-col items-center mt-10  p-3 sm:pr-52 sm:pl-52 pt-5">
+      <CardHeader>
+        <CardTitle>Fullstack web dev based in Paris</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>
+              I mainly use NextJS, TypeScript, Tailwind and SQL database for my
+              work
+            </CardTitle>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent className="pt-6">
               <p>
-                If you are coming from web3 and want to build a project i will
-                be happy to talk about it with you
+                I always pursue the latest innovations in web development. My
+                work is to create beautiful, fast, and highly secure
+                applications. Whether it's a simple website or a complex app, I
+                am thrilled to bring your vision to life.
               </p>
             </CardContent>
-          </Card>
-        </CardContent>
-      </Card>
+          </div>
+        </Card>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Contact</CardTitle>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent className="pt-6">
+              <p>Julien A.</p>
+              <p> jj.websolutions@proton.me </p>
+            </CardContent>
+          </div>
+        </Card>
+      </CardContent>
     </main>
   );
 }
--- app/workshop/page.tsx
+++ app/workshop/page.tsx
@@ -10,143 +10,141 @@ import {
 } from "@/components/ui/card";
 
 import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
 
 export default function Portfolio() {
   return (
-    <main className="flex  flex-col items-center mt-10  p-3 sm:p-16">
-      <Card className="min-w-full sm:p-12 ">
-        <CardHeader>
-          <CardTitle>What can i build for you?</CardTitle>
-          <CardDescription></CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Fullstack app</CardTitle>
-              <CardDescription>
-                Using authentification and interactions with a database
-              </CardDescription>
-            </CardHeader>
-            <div className="flex justify-between">
-              <CardContent className="pt-6">
-                <p>Card Content</p>
-              </CardContent>
-              <button className="btn btn-primary p-5">Example</button>
-            </div>
-          </Card>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Simple app</CardTitle>
-              <CardDescription>
-                Beautiful app to showcase your project
-              </CardDescription>
-            </CardHeader>
-            <div className="flex justify-between">
-              <CardContent className="pt-6">
-                <p>Card Content</p>
-              </CardContent>
-              <button className="btn btn-primary p-5">Example</button>
-            </div>
-          </Card>
+    <main className="flex  flex-col items-center mt-10  p-3  sm:pr-52 sm:pl-52">
+      <CardHeader>
+        <CardTitle>What can i build for you?</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Fullstack app</CardTitle>
+            <CardDescription>
+              Using authentification and interactions with a database
+            </CardDescription>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent className="pt-6"></CardContent>
+            <a href="#tennis" className="btn btn-primary p-5">
+              Example
+            </a>
+          </div>
+        </Card>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Simple app</CardTitle>
+            <CardDescription>
+              Beautiful website with animations to showcase your project or your
+              product with best SEO.
+            </CardDescription>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent className="pt-6"></CardContent>
+            <a href="#landing" className="btn btn-primary p-5">
+              Example
+            </a>
+          </div>
+        </Card>
 
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Wordpress/Shopify website</CardTitle>
-              <CardDescription>
-                Beautiful website accepting payments
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Wordpress/Shopify website</CardTitle>
+            <CardDescription>
+              Beautiful website accepting payments
+            </CardDescription>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent></CardContent>
+            <a href="#payment" className="btn btn-primary p-5">
+              Example
+            </a>
+          </div>
+        </Card>
+      </CardContent>
+      <Separator className="mt-10 mb-10" />
+      <CardHeader>
+        <CardTitle id="tennis">Proof of work</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Tennis reservation app</CardTitle>
+            <CardDescription>
+              This app use credential authentification and allow you to check if
+              a session is available the day you want to reserve a court. I used
+              NextJS, TypeScript, Tailwind and Prisma to build this app and a
+              SQL database
+            </CardDescription>
+          </CardHeader>
+          <div className="flex justify-center">
+            <CardContent className="pt-6 ">
+              <img
+                className="max-w-3xl hidden lg:block"
+                src="https://i.goopics.net/v9bqb3.png"
+                alt="logo"
+              />
+              <Link
+                className="flex justify-center pt-5"
+                href="https://nextcourt.vercel.app/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Try it
+              </Link>
             </CardContent>
-          </Card>
-        </CardContent>
-      </Card>
-      <Card className="mt-12 min-w-full sm:p-12">
-        <CardHeader>
-          <CardTitle>Proof of work</CardTitle>
-          <CardDescription></CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Tennis reservation app</CardTitle>
-              <CardDescription>
-                This app use credential authentification and allow you to check
-                if a session is available the day you want to reserve a court. I
-                used NextJS, TypeScript, Tailwind and Prisma to build this app
-                and a SQL database
-              </CardDescription>
-            </CardHeader>
-            <div className="flex justify-center">
-              <CardContent className="pt-6 ">
-                <img
-                  className="max-w-3xl hidden lg:block"
-                  src="https://i.goopics.net/v9bqb3.png"
-                  alt="logo"
-                />
-                <Link
-                  className="flex justify-center pt-5"
-                  href="https://nextcourt.vercel.app/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Try it
-                </Link>
-              </CardContent>
-            </div>
-          </Card>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Reddit clone</CardTitle>
-              <CardDescription>
-                This app use google authentification and allow you to create a
-                community so you and others user can create post and interact
-                with them. I used NextJS, TypeScript, Tailwind and Prisma to
-                build this app and a SQL database
-              </CardDescription>
-            </CardHeader>
-            <div className="flex justify-center">
-              <CardContent className="pt-6">
-                <img
-                  className="max-w-3xl hidden lg:block"
-                  src="https://i.goopics.net/2oi26s.png"
-                  alt="logo"
-                />
-                <Link
-                  className="flex justify-center pt-5"
-                  href="https://reddint.vercel.app/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Try it
-                </Link>
-              </CardContent>
-            </div>
-          </Card>
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Landing page</CardTitle>
-              <CardDescription>Example of a landing page ...</CardDescription>
-            </CardHeader>
-            <div className="flex justify-between">
-              <CardContent className="pt-6">
-                <p>Card Content</p>
-              </CardContent>
-            </div>
-          </Card>
+          </div>
+        </Card>
+        <Card className="w-4/4 mt-5">
+          <CardHeader>
+            <CardTitle>Reddit clone</CardTitle>
+            <CardDescription>
+              This app use google authentification and allow you to create a
+              community so you and others user can create post and interact with
+              them. I used NextJS, TypeScript, Tailwind and Prisma to build this
+              app and a SQL database
+            </CardDescription>
+          </CardHeader>
+          <div className="flex justify-center">
+            <CardContent className="pt-6">
+              <img
+                className="max-w-3xl hidden lg:block"
+                src="https://i.goopics.net/2oi26s.png"
+                alt="logo"
+              />
+              <Link
+                className="flex justify-center pt-5"
+                href="https://reddint.vercel.app/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Try it
+              </Link>
+            </CardContent>
+          </div>
+        </Card>
+        <Card className="w-4/4 mt-5">
+          <CardHeader id="landing">
+            <CardTitle>Landing page</CardTitle>
+            <CardDescription>Coming very soon</CardDescription>
+          </CardHeader>
+          <div className="flex justify-between">
+            <CardContent className="pt-6"></CardContent>
+          </div>
+        </Card>
 
-          <Card className="w-4/4 mt-5">
-            <CardHeader>
-              <CardTitle>Worpress website</CardTitle>
-              <CardDescription>coming..</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-          </Card>
-        </CardContent>
-      </Card>
+        <Card className="w-4/4 mt-5">
+          <CardHeader id="payment">
+            <CardTitle>Wordpress/Shopify website</CardTitle>
+            <CardDescription>Coming very soon</CardDescription>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </CardContent>
     </main>
   );
 }
