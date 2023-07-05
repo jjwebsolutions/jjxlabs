179e3398b95d5c28b248770ae65eef43b43e72f5
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-07-05 19:21:29 +0200

    logo and metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "jjxlabs",
-  description: "Website offering web solutions for your business",
+  description:
+    " Web agency specializing in providing creative and effective web solutions for businesses. Contact us for stunning websites, e-commerce development, and digital marketing services.",
+  keywords:
+    "web agency, web solutions, website development, e-commerce development, digital marketing,agence web, solutions web, développerment de sites web, développement d'e-commerce, marketing numérique ",
+  author: "jjxlabs",
+  creator: "Julien A",
 };
 
 export default function RootLayout({
