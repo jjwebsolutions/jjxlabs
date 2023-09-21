b141a0de946fefec1201b8770f153ba6c25273da
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-09-21 17:09:37 +0200

    design update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useInView, animated, useSpring } from "@react-spring/web";
+import Image from "next/image";
+
 import {
   Card,
   CardContent,
@@ -19,6 +22,67 @@ import scrollToTop from "@/components/scrollToTop";
 import "../app/globals.css";
 
 export default function Home() {
+  const [ref, springs] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        y: 100,
+      },
+      to: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    {
+      rootMargin: "0% 0%",
+    }
+  );
+  const [ref2, springs2] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        y: 100,
+      },
+      to: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    {
+      rootMargin: "0% 0%",
+    }
+  );
+  const [ref3, springs3] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        y: 100,
+      },
+      to: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    {
+      rootMargin: "0% 0%",
+    }
+  );
+  const [ref4, springs4] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        y: 100,
+      },
+      to: {
+        opacity: 1,
+        y: 0,
+      },
+    }),
+    {
+      rootMargin: "0% 0%",
+    }
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center   justify-between  ">
       <div className="h-screen ">
@@ -49,125 +113,225 @@ export default function Home() {
       </div>
 
       <Separator className="mt-10 mb-10" id="projects" />
-      <CardHeader>
-        <CardTitle className="cardContent2 text-xl lg:text-3xl">
-          Projects
-        </CardTitle>
-        <CardDescription></CardDescription>
-      </CardHeader>
-      <CardContent className="cardContent2">
-        <Card className="w-4/4 mt-5">
-          <CardHeader>
-            <CardTitle>Tennis reservation app</CardTitle>
-            <CardDescription>
-              This app use credential authentification and allow you to check if
-              a session is available the day you want to reserve a court. I used
-              NextJS, TypeScript, Tailwind, tRPC and Prisma to build this app
-              and a SQL database.
-            </CardDescription>
-          </CardHeader>
-          <div className="flex justify-center">
-            <CardContent className="pt-6 ">
-              <img
-                className="max-w-3xl hidden lg:block"
-                src="https://i.goopics.net/v9bqb3.png"
-                alt="screenshot of a beautiful website use for reservation"
-              />
-              <Link
-                className="flex justify-center pt-5"
-                href="https://nextcourt.vercel.app/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Try it
-              </Link>
-            </CardContent>
-          </div>
-        </Card>
-        <Card className="w-4/4 mt-5">
-          <CardHeader>
-            <CardTitle>Reddit clone</CardTitle>
-            <CardDescription>
-              This app use google authentification and allow you to create a
-              community so you and others user can create post and interact with
-              them. I used NextJS, TypeScript, Tailwind and Prisma to build this
-              app and a SQL database.
-            </CardDescription>
-          </CardHeader>
-          <div className="flex justify-center">
-            <CardContent className="pt-6">
-              <img
-                className="max-w-3xl hidden lg:block"
-                src="https://i.goopics.net/7bf8q7.png"
-                alt="screenshot of a beautiful website similar to reddit"
-              />
-              <Link
-                className="flex justify-center pt-5"
-                href="https://reddint.vercel.app/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Try it
-              </Link>
-            </CardContent>
-          </div>
-        </Card>
-        <Card className="w-4/4 mt-5">
-          <CardHeader>
-            <CardTitle>E-commerce market</CardTitle>
-            <CardDescription>
-              This app allow you to showcase and sell the products that you
-              manage in the dashboard bellow. It accepts payments via Stripe.
-              You can use the public API provide by the dashboard to link the
-              e-commerce and the dashboard.
-            </CardDescription>
-          </CardHeader>
-          <div className="flex justify-center">
-            <CardContent className="pt-6 ">
-              <img
-                className="max-w-3xl hidden lg:block"
-                src="https://i.goopics.net/y5thji.png"
-                alt="screenshot of a beautiful e-commerce website"
-              />
-              <Link
-                className="flex justify-center pt-5"
-                href="https://jjxstore-client.vercel.app/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Try it
-              </Link>
-            </CardContent>
-          </div>
-        </Card>
-        <Card className="w-4/4 mt-5">
-          <CardHeader>
-            <CardTitle>E-commerce dashboard</CardTitle>
-            <CardDescription>
-              A dashboard that allows you to manage multiple stores and their
-              products. Demo account linked to the store:
-              demojjxlabs@gmail.com:3iBPM7ActvEk7RJ
-            </CardDescription>
-          </CardHeader>
-          <div className="flex justify-center">
-            <CardContent className="pt-6 ">
-              <img
-                className="max-w-3xl hidden lg:block"
-                src="https://i.goopics.net/kkw25z.png"
-                alt="screenshot of a beautiful dashboard used to manage a store"
-              />
-              <Link
-                className="flex justify-center pt-5"
-                href="https://jjxstore-admin.vercel.app/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Try it
-              </Link>
-            </CardContent>
-          </div>
-        </Card>
+      <animated.div ref={ref} style={springs}>
+        <CardHeader>
+          <CardTitle className="cardContent2 text-xl lg:text-3xl">
+            Projects
+          </CardTitle>
+        </CardHeader>
+      </animated.div>
 
+      <CardContent className="cardContent2 p-0 space-y-10">
+        <animated.div ref={ref} style={springs}>
+          <Card className="w-4/4 mt-5">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">
+                Tennis court reservation app
+              </CardTitle>
+              <CardDescription className="lg:hidden">
+                This app use credential authentification and allow you to check
+                if a session is available the day you want to reserve a court. I
+                used NextJS, TypeScript, Tailwind, tRPC and Prisma to build this
+                app and a SQL database.
+              </CardDescription>
+            </CardHeader>
+            <div className="flex">
+              <div className="text-xl p-10 space-y-5 lg:w-1/4 w-1/2 text-center lg:block hidden">
+                <p>Next.js</p>
+                <p>TypeScript</p>
+                <p>tRPC</p>
+                <p>Prisma</p>
+                <p>Tailwind</p>
+                <p>NextAuth</p>
+              </div>
+
+              <CardContent className="pt-5 hidden lg:block  w-2/4">
+                <Image
+                  className=" "
+                  width={700}
+                  height={500}
+                  src="https://i.goopics.net/v9bqb3.png"
+                  alt="screenshot of a beautiful website use for reservation"
+                />
+              </CardContent>
+              <div className="text-xl p-10 space-y-5 text-center sm:w-1/4 w-1/2 lg:block hidden  ">
+                <p>Credential authentification</p>
+                <p>Check sessions available for the day clicked</p>
+                <p>Reserve your session easily</p>
+                <p>Admin dashboard</p>
+              </div>
+            </div>
+            <div className="flex justify-center p-5">
+              <Button className="">
+                <Link
+                  className="flex justify-center p-5"
+                  href="https://nextcourt.vercel.app/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Try it
+                </Link>
+              </Button>
+            </div>
+          </Card>
+        </animated.div>
+
+        <animated.div ref={ref3} style={springs3}>
+          <Card className="w-4/4 mt-5">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">E-commerce market</CardTitle>
+
+              <CardDescription className="lg:hidden">
+                This app allow you to showcase and sell the products that you
+                manage in the dashboard bellow. It accepts payments via Stripe.
+                You can use the public API provide by the dashboard to link the
+                e-commerce and the dashboard.
+              </CardDescription>
+            </CardHeader>
+            <div className="flex    ">
+              <div className="text-xl p-10 space-y-5 lg:w-1/4 w-1/2 text-center  lg:block hidden ">
+                <p>Next.js</p>
+                <p>TypeScript</p>
+                <p>Prisma</p>
+                <p>Tailwind</p>
+                <p>Shadcn</p>
+                <p>Clerk</p>
+              </div>
+
+              <CardContent className="pt-5 hidden lg:block  w-2/4">
+                <Image
+                  className=" "
+                  width={700}
+                  height={500}
+                  src="https://i.goopics.net/y5thji.png"
+                  alt="screenshot of a beautiful e-commerce website"
+                />
+              </CardContent>
+              <div className="text-xl p-10 space-y-5 text-center sm:w-1/4 w-1/2  lg:block hidden  ">
+                <p>Simple marketplace</p>
+                <p>People can buy the products added from your dashboard</p>
+                <p>Stripe payment</p>
+                <p>Filter products</p>
+              </div>
+            </div>
+            <div className="flex justify-center p-5">
+              <Button className="">
+                <Link
+                  className="flex justify-center p-5"
+                  href="https://jjxstore-client.vercel.app/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Try it
+                </Link>
+              </Button>
+            </div>
+          </Card>
+        </animated.div>
+        <animated.div ref={ref4} style={springs4}>
+          <Card className="w-4/4 mt-5">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">E-commerce dashboard</CardTitle>
+              <CardDescription className="lg:hidden">
+                A dashboard that allows you to manage multiple stores and their
+                products. Demo account linked to the store:
+                demojjxlabs@gmail.com:3iBPM7ActvEk7RJ
+              </CardDescription>
+              <CardDescription>
+                Demo account linked to the store:
+                demojjxlabs@gmail.com:3iBPM7ActvEk7RJ
+              </CardDescription>
+            </CardHeader>
+            <div className="flex    ">
+              <div className="text-xl p-10 space-y-5 lg:w-1/4 w-1/2 text-center  lg:block hidden ">
+                <p>Next.js</p>
+                <p>TypeScript</p>
+                <p>Prisma</p>
+                <p>Tailwind</p>
+                <p>Shadcn</p>
+                <p>Clerk</p>
+              </div>
+
+              <CardContent className="pt-5 hidden lg:block  w-2/4">
+                <Image
+                  className=" "
+                  width={700}
+                  height={500}
+                  src="https://i.goopics.net/kkw25z.png"
+                  alt="screenshot of a beautiful dashboard used to manage a store"
+                />
+              </CardContent>
+              <div className="text-xl p-10 space-y-5 text-center sm:w-1/4 w-1/2   lg:block hidden ">
+                <p>Google authentification</p>
+                <p>Manage multiple stores</p>
+                <p>Add products to the stores</p>
+                <p>Manage the orders from your stores</p>
+              </div>
+            </div>
+            <div className="flex justify-center p-5">
+              <Button className="">
+                <Link
+                  className="flex justify-center p-5"
+                  href="https://jjxstore-admin.vercel.app/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Try it
+                </Link>
+              </Button>
+            </div>
+          </Card>
+        </animated.div>
+        <animated.div ref={ref2} style={springs2}>
+          <Card className="w-4/4 mt-5">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">Reddit clone</CardTitle>
+              <CardDescription className="lg:hidden">
+                This app use google authentification and allow you to create a
+                community so you and others user can create post and interact
+                with them. I used NextJS, TypeScript, Tailwind and Prisma to
+                build this app and a SQL database.
+              </CardDescription>
+            </CardHeader>
+            <div className="flex    ">
+              <div className="text-xl p-10 space-y-5 lg:w-1/4 w-1/2 text-center  lg:block hidden">
+                <p>Next.js</p>
+                <p>TypeScript</p>
+                <p>Prisma</p>
+                <p>Tailwind</p>
+                <p>Shadcn</p>
+                <p>ClerkAuth</p>
+              </div>
+
+              <CardContent className="pt-5 hidden lg:block  w-2/4">
+                <Image
+                  className=" "
+                  width={700}
+                  height={500}
+                  src="https://i.goopics.net/7bf8q7.png"
+                  alt="screenshot of a beautiful website similar to reddit"
+                />
+              </CardContent>
+              <div className="text-xl p-10 space-y-5 text-center sm:w-1/4 w-1/2  lg:block hidden ">
+                <p>Google authentification</p>
+                <p>Create a community so people can join it</p>
+                <p>Add posts to the community so users can interact with it</p>
+              </div>
+            </div>
+            <div className="flex justify-center p-5">
+              <Button className="">
+                <Link
+                  className="flex justify-center p-5"
+                  href="https://reddint.vercel.app/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Try it
+                </Link>
+              </Button>
+            </div>
+          </Card>
+        </animated.div>
         <Card className="w-4/4 mt-5">
           <CardHeader id="payment">
             <CardTitle>AI SaaS platform</CardTitle>
@@ -176,7 +340,7 @@ export default function Home() {
           <CardContent></CardContent>
         </Card>
       </CardContent>
-      <Separator />
+      <Separator className="mt-20 mb-10" />
       <div className="h-screen sm:p-14 mt-10" id="about">
         <CardHeader>
           <CardTitle className="text-xl lg:text-3xl text-center">
