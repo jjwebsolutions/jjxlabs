cce9302f5606facbed95af1349a4dec7ec1363aa
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-10-08 03:06:27 +0200

    Update ContentPageAnimated.tsx

--- components/ContentPageAnimated.tsx
+++ components/ContentPageAnimated.tsx
@@ -171,7 +171,7 @@ function ContentPageAnimated() {
                 <CardHeader className="text-center">
                   <CardTitle>Tailwind / Redux / Shadcn</CardTitle>
                   <CardDescription className="text-center h-16 sm:h-5">
-                    Making my app simple but elegant
+                    Making my apps simple but elegant
                   </CardDescription>
                 </CardHeader>
               </Card>
