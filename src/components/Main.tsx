b382086a421a6a207d4f012b08413e7a45434d30
Atanassov <jj.websolutions@proton.me>
2024-07-04 23:40:00 +0200

    link

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -176,7 +176,10 @@ export function Main() {
           >
             Terms of Service
           </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
+          <Link
+            className="text-xs hover:underline underline-offset-4"
+            href="/privacy"
+          >
             Privacy
           </Link>
         </nav>
