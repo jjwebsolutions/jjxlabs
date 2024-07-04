fd31fb0da84f876574d49c08dde364c3cc0f123d
Atanassov <jj.websolutions@proton.me>
2024-07-04 20:15:04 +0200

    meatadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,6 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
